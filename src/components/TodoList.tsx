8134d80bb45e361f116fae77d3e1aaf22399ea7b
rudwnok123 <rudwnok123@naver.com>
2021-04-13 01:31:52 +0900

    commit test

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,7 +7,7 @@ function TodoList() {
   const todos = useTodos(); // TODO: 커스텀 Hook 사용하여 조회
 
   if (todos.length === 0) return <p>등록된 항목이 없습니다.</p>;
-  
+    
   return (
     <ul>
       {todos.map(todo => (
