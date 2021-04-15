103ab770487452ed02fc78ad4171ca4323788e59
DESKTOP-U42C94B\master <r>
2021-04-15 16:11:36 +0900

    주석

--- src/component/TodoForm.tsx
+++ src/component/TodoForm.tsx
@@ -7,6 +7,8 @@ const TodoForm = () => {
   return (
     <section className="todoapp">
       {/* TODO: 헤더에서 인풋검색값을 TodoList 로 보내기 */}
+      {/* 1. redux는 다음 단계에.. */}
+      {/* 2. header 에서 데이터 올리고 TodoForm 에서 다시 데이터 내리기 */}
       <Header />
       <TodoList /> 
       <Footer />
