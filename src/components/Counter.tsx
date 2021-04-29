5e063eac0e1988d66838434f329845c7d585386d
rudwnok123 <rudwnok123@naver.com>
2021-04-29 21:37:18 +0900

    scss module test

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -18,7 +18,7 @@ function Counter({
       <h1>{count}</h1>
       <button onClick={onIncrease}>+1</button>
       <button onClick={onDecrease}>-1</button>
-      <button onClick={() => onIncreaseBy(5)}>+5</button>
+      <button onClick={e => location.href='url'}>+5</button>
     </div>
   );
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './TodoItem.css';
+import styles from './TodoItem.module.scss';
 import { Todo } from '../modules/todos';
 
 type TodoItemProps = {
@@ -12,6 +12,10 @@ function TodoItem({ todo }: TodoItemProps) {
     <li className={`TodoItem ${todo.done ? 'done' : ''}`}>
       <span className="text">{todo.text}</span>
       <span className="remove">(X)</span>
+      <div className={styles.TodoItem__text}>asdfasdf</div>
+      {
+        console.log('styles', styles)
+      }
     </li>
   );
 }
